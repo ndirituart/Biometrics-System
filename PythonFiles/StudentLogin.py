90c4c5c7fcf5b606abd1cd40e5be13dabf89e721
Patience <105917777+ndirituart@users.noreply.github.com>
2024-05-13 11:09:33 +0300

    Update StudentLogin.py
    Student login page with Student names and Admission number as verifications, I haven't put a password because the fingerprint later on caters for that. Haven't connected to a database yet, so my control for logging in is an identified user Patience Ndiritu and admission number 21-2077

--- PythonFiles/StudentLogin.py
+++ PythonFiles/StudentLogin.py
@@ -1,7 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-import cv2
-import os
 
 root = Tk()
 root.title("Student Login")
@@ -10,20 +8,13 @@ root.configure(bg="#fff")
 root.resizable(False, False)
 
 # Function that logs in students with constraints of CORRECT Studentname and Admission number
-def signIn(user=None, code=None):
-    Studentname = user.get()
-    Admissionnumber = code.get()
+def signIn(user_entry, code_entry):
+    Studentname = user_entry.get()
+    Admissionnumber = code_entry.get()
 
     # Control function to login a saved user. This will be removed once we have a database loaded with Student names and admission numbers
     if Studentname == "Patience Njeri" and Admissionnumber == '2077':
-        screen = Toplevel(root)
-        screen.title("Verification")
-        screen.geometry('925x500+300+200')
-        screen.config(background="white")
-
-        Label(screen, text="Welcome Patience :)!", bg="red", fg="white", font=('Calibri', 50, "bold")).pack()
-
-        screen.mainloop()
+        messagebox.showinfo("Success", "Welcome Patience Njeri!")
 
     elif Studentname != 'Patience Njeri' and Admissionnumber == '2077':
         messagebox.showerror("Invalid", "Invalid Student name and Admission number")
@@ -34,6 +25,9 @@ def signIn(user=None, code=None):
     elif Studentname != "Patience Njeri":
         messagebox.showerror("Invalid", "Invalid Student name")
 
+def Login():
+    signIn(user, code)
+
 image = PhotoImage(file='Login.png')
 Label(root, image=image, bg='white').place(x=50, y=50)
 
@@ -43,16 +37,15 @@ frame.place(x=480, y=70)
 heading = Label(frame, text="Login", bg="light grey", fg="#57a1f8", font=('Microsoft Yattei UI Light', 23, 'bold'))
 heading.place(x=100, y=5)
 
-# ---------------------------------------------------------
-def on_enter(e, user=None, end=None):
-    user.delete(0,end)
+def on_enter(e, widget='', end=None):
+    widget.delete(0,end)
 
-def on_leave(e, user=None):
-    name=user.get()
+def on_leave(e, widget=''):
+    name = widget.get()
     if name == '':
-        user.insert(0,'Studentname')
+        widget.insert(0, 'Studentname')
 
-user= Entry(frame, width=25, fg='black', border=0, bg="white", font=('Microsoft Yattei UI Light', 11, "bold"))
+user = Entry(frame, width=25, fg='black', border=0, bg="white", font=('Microsoft Yattei UI Light', 11, "bold"))
 user.place(x=30,y=80)
 user.insert(0,'Studentname')
 user.bind('<FocusIn>', on_enter)
@@ -60,22 +53,7 @@ user.bind('<FocusOut>', on_leave)
 
 Frame(frame, width=295, height=2, bg="black").place(x=25, y=107)
 
-user=Entry(frame, width=25, fg='black', border=2,bg='white', font=('Microsoft Yattei UI Light',11, "bold"))
-user.place(x=30,y=80)
-user.insert(0,'Studentname')
-
-Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
-
-###########---------------------------------------------------------------------------
-def on_enter(e, code=None, end=None):
-    code.delete(0,end)
-
-def on_leave(e, code=None):
-    name=code.get()
-    if name == '':
-            code.insert(0,'Admissionnumber')
-
-code= Entry(frame, width=25, fg='black', border=2, bg='white',font=('Microsoft Yattei UI Light',11, "bold"))
+code = Entry(frame, width=25, fg='black', border=2, bg='white',font=('Microsoft Yattei UI Light',11, "bold"))
 code.place(x=30,y=150)
 code.insert(0,'Admission number')
 code.bind('<FocusIn>', on_enter)
@@ -83,12 +61,6 @@ code.bind('<FocusOut>', on_leave)
 
 Frame(frame, width=295, height=2, bg='black').place(x=25,y=177)
 
-code=Entry(frame, width=25, fg='black', border=2, bg='white', font=('Microsoft Yattei UI Light',11, "bold"))
-code.place(x=30,y=150)
-code.insert(0,'Admission number')
+Button(frame, width=39, pady=7,text='Login', bg='#57a1f8',fg='white',border=0, command=Login).place(x=35,y=204)
 
-Frame(frame, width=295, height=2, bg='black').place(x=25,y=177)
-
-#################------------------------------------------------------------------
-Button(frame, width=39, pady=7,text='Login', bg='#57a1f8',fg='white',border=0, command=signIn).place(x=35,y=204)
 root.mainloop()
